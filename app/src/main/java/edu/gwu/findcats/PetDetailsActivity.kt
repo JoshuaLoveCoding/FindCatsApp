c37d84317948d8852b7e0ad74c934d09c215a92c
JoshuaLoveCoding <anqi6591@gwu.edu>
2018-10-01 15:52:38 -0400

    Third commit

---
+++ app/src/main/java/edu/gwu/findcats/PetDetailsActivity.kt
@@ -0,0 +1,12 @@
+package edu.gwu.findcats
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class PetDetailsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pet_details)
+    }
+}
---
+++ app/src/main/java/edu/gwu/findcats/PetsActivity.kt
@@ -0,0 +1,12 @@
+package edu.gwu.findcats
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class PetsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pets)
+    }
+}
